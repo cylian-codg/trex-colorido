b1a2ce6fdab609d6d2af1601acff08eb754b8048
Cylian Lins Mendes Melo <82554933+cylian-codg@users.noreply.github.com>
2022-09-28 12:45:18 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,129 +1,190 @@
-  
-var towerImg, tower;
-var doorImg, door, doorsGroup;
-var climberImg, climber, climbersGroup;
-var ghost, ghostImg;
-var invisibleBlockGroup, invisibleBlock;
-var gameState = "play"
+//estado de jogo
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+//grupo de nuvens
+var cloudsGroup, cloudImage;
+///grupo de obstaculos
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score;
+
+var gameOverImg,restartImg;
 
 
 function preload(){
-  towerImg = loadImage("tower.png");
-  doorImg = loadImage("door.png");
-  climberImg = loadImage("climber.png");
-  ghostImg = loadImage("ghost-standing.png");
-  spookySound = loadSound("spooky.wav");
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  
+  groundImage = loadImage("ground2.png");
+ 
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+  
+   restartImg = loadImage("restart.png")
+  gameOverImg = loadImage("gameOver.png")
+  
 }
 
 function setup() {
-  createCanvas(600,600);
-  //spookySound.loop();
-  tower = createSprite(300,300);
-  tower.addImage("tower",towerImg);
-  tower.velocityY = 1;
+  createCanvas(600, 200);
   
-  doorsGroup = new Group();
-  climbersGroup = new Group();
-  invisibleBlockGroup = new Group();
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
   
-  ghost = createSprite(200,200,50,50);
-  ghost.scale = 0.3;
-  ghost.addImage("ghost", ghostImg);
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //criar grupos de obstáculos e nuvens
+  obstaclesGroup = createGroup();
+  cloudsGroup = createGroup();
+  
+  console.log("Olá" + 5);
+  
+  score = 0;
 }
 
-
 function draw() {
-  background(255);
- if(tower.y > 400 ){
-      tower.y = 300
-    } 
+  background(180);
+  //exibindo pontuação
+  text("Pontuação: "+ score, 500,50);
   
-  if (gameState === "play") {
+  
+  //comportamentos do play
+  if(gameState === PLAY){
+
+     gameOver.visible = false
+    restart.visible = false
+    //mover o solo
+    ground.velocityX = -4;
+    //pontuação
+    score = score + Math.round(frameCount/60);
     
-    if(keyDown(LEFT_ARROW)){
-        ghost.x = ghost.x - 3;
-
-      // escreva o código para mover para a esquerda quando a tecla para a esquerda for pressionada
-    }
-    if(keyDown(RIGHT_ARROW)){
-  
-          ghost.x = ghost.x + 3;
-
-      // escreva o código para mover para a direita quando a tecla para a direita for pressionada
-      
-    }
-    if(keyDown("space")){
-  
-         ghost.velocityY = -10;
-
-      // escreva o código para mover para cima quando a tecla espaço for pressionada
-      
-    }
-  
-  ghost.velocityY = ghost.velocityY + 0.8;
-  
-   
-      //escreva uma condição para a torre de rolagem infinita
-    
-      spawnDoors();
-
-  
-//escreva um código para fazer invisibleBlockGroup (grupo de bloco invisível) colidir com o fantasma, destruir o fantasma e mudar o estado do jogo para end.
-     if(climbersGroup.isTouching(ghost)){
-      ghost.velocityY = 0;
-    }
-    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
-      ghost.destroy();
-      gameState = "end"
+    if (ground.x < 0){
+      ground.x = ground.width/2;
     }
     
+    //pular quando a tecla espaço for pressionada
+    if(keyDown("space")&& trex.y >= 100) {
+        trex.velocityY = -13;
+    }
+    
+    //adicionar gravidade
+    trex.velocityY = trex.velocityY + 0.8
+  
+    //gerar as nuvens
+    spawnClouds();
+  
+    //gerar obstáculos no chão
+    spawnObstacles();
+    
+    //mudança de estado de jogo
+    if(obstaclesGroup.isTouching(trex)){
+        gameState = END;
+    }
+
+  }//comportamento do play
+   else if (gameState === END) {
+      ground.velocityX = 0;
+
+      gameOver.visible = true;
+      restart.visible = true;
+
+      ground.velocityX = 0;
+      trex.velocityY = 0
+
+      //mudar a animação do trex
+      trex.changeAnimation("collide", trex_collided);
+
+      //definir tempo de vida aos objetos do jogo para que nunca sejam destruídos
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+
+      //obstaculos teram uma mudança na velocidade X em todos para 0
+     obstaclesGroup.setVelocityXEach(0);
+     cloudsGroup.setVelocityXEach(0);
+   }
+  
+ 
+  //impedir que o trex caia
+  trex.collide(invisibleGround);
+  
+  
   
   drawSprites();
 }
-  if (gameState === "end"){
-    stroke("yellow");
-    fill("yellow");
-    textSize(30);
-    text("Game Over", 230,250)
-  }
+
+function spawnObstacles(){
+ if (frameCount % 60 === 0){
+   var obstacle = createSprite(400,165,10,40);
+   obstacle.velocityX = -6;
+   
+    //gerar obstáculos aleatórios
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+   
+    //atribua dimensão e tempo de vida aos obstáculos              
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+   
+   //adicione cada obstáculo ao grupo
+    obstaclesGroup.add(obstacle);
+ }
 }
 
-function spawnDoors()
- {
+function spawnClouds() {
   //escreva o código aqui para gerar as nuvens
-  if (frameCount % 240 === 0) {
-    var x = random(100,500)
-    var door = createSprite(x, -50);
-    var climber = createSprite(x,10);
-    var invisibleBlock = createSprite(x,15);
-    invisibleBlock.width = climber.width;
-    invisibleBlock.height = 2;
-    //adicione a função aleatória
+   if (frameCount % 60 === 0) {
+     cloud = createSprite(600,100,40,10);
+    cloud.y = Math.round(random(10,60));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
     
-    door.addImage(doorImg);
-    climber.addImage(climberImg);
+     //atribua tempo de vida à variável
+    cloud.lifetime = 134;
     
-    door.velocityY = 1;
-    climber.velocityY = 1;
-    invisibleBlock.velocityY = 1;
-
-    //mude a profundidade do fantasma e da porta
+    //ajustar a profundidade
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
     
-     
-ghost.depth = door.depth;
-    ghost.depth =1;
-    
-    //atribuir tempo de vida para a porta, escalador e bloco invisível
-
-    door.lifetime = 800;
-    climber.lifetime = 800;
-    invisibleBlock.lifetime = 800;
-    //adicione cada obstáculo ao grupo obstaclesGroup.add(obstacle); aqui os obstáculos são as portas, o escalador e o bloco invisível
-    
-     doorsGroup.add(door);
-    invisibleBlock.debug = true;
-    climbersGroup.add(climber);
-    invisibleBlockGroup.add(invisibleBlock);
-  }
+    //adicionando nuvens ao grupo
+   cloudsGroup.add(cloud);
+    }
 }
 
